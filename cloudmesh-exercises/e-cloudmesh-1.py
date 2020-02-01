651d0f3f79a30e70de24957a070bd219056c375a
Ashok Reddy Singam <31090629+asingam2017@users.noreply.github.com>
2020-01-31 21:53:36 -0800

    Update e-cloudmesh-1.py

--- cloudmesh-exercises/e-cloudmesh-1.py
+++ cloudmesh-exercises/e-cloudmesh-1.py
@@ -1,1 +1,18 @@
+# sp20-516-232 E.Cloudmesh.Common.1 ---- Work inprogress
 
+from cloudmesh.common.util import banner
+from cloudmesh.common.debug import VERBOSE
+from cloudmesh.common.util import HEADING
+
+# banner
+banner('Welcome to sp20-516-232 Class!')
+
+# verbos
+m = {'ZipCode': '90503'}
+VERBOSE(m)
+
+#heading
+class Example(object):
+    def doit(self):
+        HEADING()
+        print ('Hello')
