b6761e1d179a6fd7acf902dfcaf586a39ce75df3
Ashok Reddy Singam <31090629+asingam2017@users.noreply.github.com>
2020-02-01 23:15:13 -0800

    Update e-cloudmesh-1.py

--- cloudmesh-exercises/e-cloudmesh-1.py
+++ cloudmesh-exercises/e-cloudmesh-1.py
@@ -1,18 +1,22 @@
-# sp20-516-232 E.Cloudmesh.Common.1 ---- Work inprogress
-
+# sp20-516-232 E.Cloudmesh.Common.1 
 from cloudmesh.common.util import banner
 from cloudmesh.common.debug import VERBOSE
 from cloudmesh.common.util import HEADING
+from cloudmesh.common.variables import Variables
+
+variables = Variables()
+
+variables['debug'] = True
+variables['trace'] = True
+variables['verbose'] = 10
 
 # banner
+print ('')
 banner('Welcome to sp20-516-232 Class!')
 
-# verbos
+#verbose
 m = {'ZipCode': '90503'}
 VERBOSE(m)
 
 #heading
-class Example(object):
-    def doit(self):
-        HEADING()
-        print ('Hello')
+HEADING(txt='Welcome to sp20-516-232 Class!', c='#', color='HEADER')
