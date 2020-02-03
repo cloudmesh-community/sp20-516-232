2038414c23566c2d15a6d7cb7e0881369891fb5d
Ashok Reddy Singam <31090629+asingam2017@users.noreply.github.com>
2020-02-02 18:30:36 -0800

    Week3 updates

--- cloudmesh-exercises/e-cloudmesh-2.py
+++ cloudmesh-exercises/e-cloudmesh-2.py
@@ -1,11 +1,10 @@
 # sp20-516-232 E.Cloudmesh.Common.2: DotDic
 from cloudmesh.common.dotdict import dotdict
+from cloudmesh.common.util import banner
 
-data = {
-    'name': 'Ashok Singam',
-    'course':'sp20-516-232'
-}
+person = {'name': 'John Doe', 'age': '40', 'sex': 'Male'}
 
-data = dotdict(data)
-print(data.name)
-print(data.course)
+data = dotdict(person)
+
+banner('Demo dot dictionary')
+print(data.name+' '+data.age+' '+data.sex)
